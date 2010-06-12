0a74c4f9183b1dd2c712f3c0ac50c8a268500ca7
dbr <dbr.onix@gmail.com>
2010-06-12 22:10:48 +0930

    More complete basic test case

--- imdb/test/test_basic.py
+++ imdb/test/test_basic.py
@@ -3,4 +3,7 @@ import imdb
 
 def test_simple_search():
     sr = imdb.search("ghost busters")
-    print sr
+    first = sr['data']['results'][0]['list'][0]
+    print first
+    assert first['title'] == 'Ghost Busters'
+    assert first['tconst'] == 'tt0087332'
